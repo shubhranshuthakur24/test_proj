5a8248fc92d0c231c644dabe86ed532fd393f7a4
Shubhranshu Thakur <shubhranshuthakur24@gmail.com>
2022-02-19 14:18:17 +0530

    attempt 3

--- lib/test.dart
+++ lib/test.dart
@@ -1,25 +1,51 @@
 import 'package:flutter/material.dart';
 
-class TestPage extends StatefulWidget {
+// class TestPage extends StatefulWidget {
+//   const TestPage({Key? key}) : super(key: key);
+
+//   @override
+//   _TestPageState createState() => _TestPageState();
+// }
+
+// class _TestPageState extends State<TestPage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Container(
+//         child: Text(
+//           'Givers Health',
+//           style: TextStyle(
+//             fontSize: 40,
+//             color: Colors.red,
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+class TestPage extends StatelessWidget {
   const TestPage({Key? key}) : super(key: key);
 
   @override
-  _TestPageState createState() => _TestPageState();
-}
-
-class _TestPageState extends State<TestPage> {
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Text(
-          'Givers Health',
-          style: TextStyle(
-            fontSize: 40,
-            color: Colors.red,
-          ),
-        ),
-      ),
+    return Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [Colors.purple, Colors.orange])),
+      child: Scaffold(
+          // By defaut, Scaffold background is white
+          // Set its value to transparent
+          backgroundColor: Colors.transparent,
+          body: Center(
+            child: Container(
+              width: 200,
+              height: 200,
+              color: Colors.white,
+            ),
+          )),
     );
   }
 }
